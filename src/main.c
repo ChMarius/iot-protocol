7292eb7a2c944aeb0f4d97030ec05bbdd27eef74
Chirita Marius <chiritamarius818@gmail.com>
2026-04-28 11:39:15 +0200

    additional changes

--- src/main.c
+++ src/main.c
@@ -1,13 +1,24 @@
 #include <zephyr/kernel.h>
 #include <zephyr/usb/usb_device.h>
+#include <zephyr/net/socket.h>
 
 int main(void)
 {
     usb_enable(NULL);
     k_msleep(2000);
 
+    printk("Testing raw socket support...\n");
+
+    int sock = zsock_socket(AF_INET, SOCK_RAW, 253);
+
+    if (sock < 0) {
+        printk("FAIL: raw socket not supported, errno=%d\n", errno);
+    } else {
+        printk("SUCCESS: raw socket opened, fd=%d\n", sock);
+        zsock_close(sock);
+    }
+
     while (1) {
-        printk("Pico W heartbeat\n");
         k_msleep(1000);
     }
 
